8d63955fd3a61bc8bc7febbb7e191d5c9879fb8c
QANSIGLIERE <qansigliere@gmail.com>
2025-01-30 14:21:14 +0300

    JS-57

---
+++ dist/axios_utils.d.ts
@@ -0,0 +1,5 @@
+export function get(base_url: any, additional_path: any, headers: any, debug?: boolean): Promise<any>;
+export function download(base_url: any, additional_path: any, headers: any): Promise<any>;
+export function post(base_url: any, additional_path: any, json_body: any, headers: any): Promise<any>;
+export function post_file(base_url: any, additional_path: any, path_to_file: any, headers: any, file_key?: string): Promise<any>;
+//# sourceMappingURL=axios_utils.d.ts.map
